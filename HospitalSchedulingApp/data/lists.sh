2de28dd09c82fcda6e0ef96109b6d721d2ad3fc4
Ashleen <a.chivanga@alustudent.com>
2023-08-05 12:39:43 -0700

    A lists.sh file changed

---
+++ HospitalSchedulingApp/data/lists.sh
@@ -0,0 +1,17 @@
+Names of Available Hospitals
+1.Kibagabaga Hospital
+2. University Teaching Hospital of Kigali
+3. Kacyiyu Hospital
+4. Muhima Health Center
+5. La Croix du Sud Hospital
+6. Proposed General Hospital
+7. Nyarugenge District Hospital
+8. Ubuzima Hospital
+9. Omega Hospital
+10. Mbc HospitalNameof Available hospitals
+Available Time slots
+*Hospitals Operate 24hrs
+Specialists
+All Registered specialists
+
+
