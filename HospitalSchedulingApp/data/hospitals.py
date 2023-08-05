7582968a2bee898ea324ca7e2a964c779679d053
cekwedike <c.ekwedike@alustudent.com>
2023-08-05 11:10:16 -0700

    editing and corrections

--- HospitalSchedulingApp/data/hospitals.py
+++ HospitalSchedulingApp/data/hospitals.py
@@ -105,9 +105,11 @@ if __name__ == "__main__":
     appointment_details = schedule_appointment()
 
     print("\nThank you for scheduling your appointment!" + " " + "Your Appointment Has Been Booked :)")
+    print("\n ")
     print("Appointment Summary:")
-    print("*************************||***********************||*********************||*****************")
+    print("*************************||***********************||************************||")
     print(f"Hospital: {selected_hospital}")
+    print("*************************||***********************||************************||")
     print(f"Department: {selected_department}")
     if "Specialist" in appointment_details:
         print(appointment_details)
